dfb620c5e6c13f9cdbb21f6434d9a4250bb390a7
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-11 22:25:32 +0700

    red: test mom is valid palindrome

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,9 @@
+import { describe, it, expect } from "@jest/globals";
 
-describe('palindrome checker', () => {
-
-})
+describe("palindrome checker", () => {
+  it('should return true when input string is "mom"', () => {
+    const palindromeChecker = new PalindromeChecker();
+    const result = palindromeChecker.check("mom");
+    expect(result).toBeTruthy();
+  });
+});
