9ece21d28aa18c780d077d97c7cfe6efd56e602a
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-11 22:45:29 +0700

    refactor: initialize class instance

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,27 +1,27 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, beforeEach } from "@jest/globals";
 import PalindromeChecker from "./index";
 
 describe("palindrome checker", () => {
+  let palindromeChecker: PalindromeChecker;
+  beforeEach(() => {
+    palindromeChecker = new PalindromeChecker();
+  });
   it('should return true when input string is "mom"', () => {
-    const palindromeChecker = new PalindromeChecker();
     const result = palindromeChecker.check("mom");
     expect(result).toBeTruthy();
   });
 
   it('should return true when input string is "Mom"', () => {
-    const palindromeChecker = new PalindromeChecker();
     const result = palindromeChecker.check("Mom");
     expect(result).toBeTruthy();
   });
 
   it('should return true when input string is "MoM"', () => {
-    const palindromeChecker = new PalindromeChecker();
     const result = palindromeChecker.check("MoM");
     expect(result).toBeTruthy();
   });
 
   it('should return false when input string is "Momx"', () => {
-    const palindromeChecker = new PalindromeChecker();
     const result = palindromeChecker.check("Momx");
     expect(result).toBeFalsy();
   });
