7e68f795b23c70c193072b78c80f1b4f8c215099
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-11 23:04:55 +0700

    refactor: reduce duplicate test

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -3,46 +3,22 @@ import PalindromeChecker from "./index";
 
 describe("palindrome checker", () => {
   let palindromeChecker: PalindromeChecker;
+
   beforeEach(() => {
     palindromeChecker = new PalindromeChecker();
   });
-  it('should return true when input string is "mom"', () => {
-    const result = palindromeChecker.check("mom");
-    expect(result).toBeTruthy();
-  });
 
-  it('should return true when input string is "Mom"', () => {
-    const result = palindromeChecker.check("Mom");
-    expect(result).toBeTruthy();
-  });
-
-  it('should return true when input string is "MoM"', () => {
-    const result = palindromeChecker.check("MoM");
-    expect(result).toBeTruthy();
-  });
-
-  it('should return false when input string is "Momx"', () => {
-    const result = palindromeChecker.check("Momx");
-    expect(result).toBeFalsy();
-  });
-
-  it('should return true when input string is "Was It A Rat I Saw"', () => {
-    const result = palindromeChecker.check("Was It A Rat I Saw");
-    expect(result).toBeTruthy();
-  });
-
-  it('should return true when input string is "Never Odd or Even"', () => {
-    const result = palindromeChecker.check("Never Odd or Even");
-    expect(result).toBeTruthy();
-  });
-
-  it('should return true when input string is "Never Odd or Even1"', () => {
-    const result = palindromeChecker.check("Never Odd or Even1");
-    expect(result).toBeFalsy();
-  });
-
-  it('should return true when input string is "1Never Odd or Even1"', () => {
-    const result = palindromeChecker.check("1Never Odd or Even1");
-    expect(result).toBeTruthy();
+  it.each([
+    ["mom", true],
+    ["Mom", true],
+    ["MoM", true],
+    ["Momx", false],
+    ["Was It A Rat I Saw", true],
+    ["Never Odd or Even", true],
+    ["Never Odd or Even1", false],
+    ["1Never Odd or Even1", true],
+  ])('should return %s when input string is "%s"', (input, expected) => {
+    const result = palindromeChecker.check(input);
+    expect(result).toBe(expected);
   });
 });
