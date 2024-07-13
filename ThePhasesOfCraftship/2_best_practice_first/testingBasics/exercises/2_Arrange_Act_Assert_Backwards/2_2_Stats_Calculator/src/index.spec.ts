776f70db6b4a4416f494cd8537ba6c2e5f26e157
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-13 23:06:41 +0700

    green: implement get min from sequence

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "@jest/globals";
+import StatsCalculator from "./index";
 describe("stats calculator", () => {
   it("should return -8 as minimum value from sequence [2, 4, 21, -8, 53, 40]", () => {
     // arrange
---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,26 @@
+type Sequence = number[];
+
+class StatsCalculator {
+  sequence: Sequence;
+
+  constructor(sequence: Sequence) {
+    this.sequence = sequence;
+  }
+
+  public min() {
+    let result;
+
+    for (const n of this.sequence) {
+      if (result === undefined) {
+        result = n;
+      }
+      if (n < result) {
+        result = n;
+      }
+    }
+
+    return result;
+  }
+}
+
+export default StatsCalculator;
