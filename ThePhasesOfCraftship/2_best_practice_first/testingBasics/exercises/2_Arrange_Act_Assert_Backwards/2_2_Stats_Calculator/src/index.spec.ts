76636f60c7850efdd663ab817114a20afd8179d0
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-13 22:54:36 +0700

    red: test min from sequence

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,12 @@
-
-describe('stats calculator', () => {
-
-
-})
+import { describe, expect, it } from "@jest/globals";
+describe("stats calculator", () => {
+  it("should return -8 as minimum value from sequence [2, 4, 21, -8, 53, 40]", () => {
+    // arrange
+    const sequence = [2, 4, 21, -8, 53, 40];
+    const statsCalculator = new StatsCalculator(sequence);
+    // act
+    const result = statsCalculator.min();
+    // assert
+    expect(result).toEqual(-8);
+  });
+});
