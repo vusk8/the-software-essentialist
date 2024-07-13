c111579ef665340c65ccf0d123d7ba0fd5da9035
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-13 14:48:15 +0700

    green: test short password

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -12,4 +12,17 @@ describe("password validator", () => {
     expect(result.isValid).toBeTruthy();
     expect(result.errors).toEqual([]);
   });
+
+  test("should return isValid false and errors for 1234 password", () => {
+    // arrange
+    const inputPassword = "1234";
+    const passwordValidator = new PasswordValidator();
+    // act
+    const result = passwordValidator.validate(inputPassword);
+    // assert
+    expect(result.isValid).toBeFalsy();
+    expect(result.errors[0]).toEqual(
+      "Password length must be between 5 and 15 characters"
+    );
+  });
 });
