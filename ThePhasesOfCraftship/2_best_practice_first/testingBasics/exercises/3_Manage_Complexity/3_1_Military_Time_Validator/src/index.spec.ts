94019be913d97e871b6b6e18eeea8b3ef6793339
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-21 17:21:33 +0700

    refactor: parameterize test

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,21 +2,21 @@ import { describe, it, expect } from "@jest/globals";
 import MilitaryTimeValidator from "./index";
 
 describe("military time validator", () => {
-  it("should return 'yes' when validating '01:12 - 14:32'", () => {
-    const militaryTimeValidator = new MilitaryTimeValidator("01:12 - 14:32");
-    const result = militaryTimeValidator.validateTimeRange();
-    expect(result).toEqual("yes");
-  });
+  const testCases = [
+    ["01:12 - 14:32", "yes"],
+    ["22:00 - 23:12", "yes"],
+    ["25:00 - 12:23", "no"],
+    ["24:00 - 00:00", "no"],
+    ["12:00 - 12:00", "no"],
+    ["12:59 - 12:00", "no"],
+  ];
 
-  it("should return 'yes' when validating '22:00 - 23:12'", () => {
-    const militaryTimeValidator = new MilitaryTimeValidator("22:00 - 23:12");
-    const result = militaryTimeValidator.validateTimeRange();
-    expect(result).toEqual("yes");
-  });
-
-  it("should return 'no' when validating '25:00 - 12:23'", () => {
-    const militaryTimeValidator = new MilitaryTimeValidator("25:00 - 12:23");
-    const result = militaryTimeValidator.validateTimeRange();
-    expect(result).toEqual("no");
-  });
+  it.each(testCases)(
+    "should return '%s' when validating '%s'",
+    (timeRange, expected) => {
+      const militaryTimeValidator = new MilitaryTimeValidator(timeRange);
+      const result = militaryTimeValidator.validateTimeRange();
+      expect(result).toEqual(expected);
+    }
+  );
 });
