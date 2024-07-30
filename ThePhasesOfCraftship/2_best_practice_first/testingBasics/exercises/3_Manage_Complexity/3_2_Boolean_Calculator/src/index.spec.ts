9d5e846ffe850ce2d557e777f0630677af2852d2
Huynh Hoan Vu <vuhh5@fpt.com>
2024-07-30 07:32:03 +0700

    feat: mock test for or and operator

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,28 +1,55 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, beforeEach } from "@jest/globals";
 import { BooleanCalculator } from "./index";
 
 describe("boolean calculator", () => {
+  let booleanCalculator: BooleanCalculator;
+
+  beforeEach(() => {
+    booleanCalculator = new BooleanCalculator();
+  });
+
   it('should return true when evaluate "TRUE" expression', () => {
-    const booleanCalculator = new BooleanCalculator();
-
     const result = booleanCalculator.evaluate("TRUE");
-
     expect(result).toBeTruthy();
   });
 
   it('should return false when evaluate "FALSE" expression', () => {
-    const booleanCalculator = new BooleanCalculator();
-
     const result = booleanCalculator.evaluate("FALSE");
-
     expect(result).toBeFalsy();
   });
 
   it('should return false when evaluate "NOT TRUE" expression', () => {
-    const booleanCalculator = new BooleanCalculator();
-
     const result = booleanCalculator.evaluate("NOT TRUE");
+    expect(result).toBeFalsy();
+  });
 
+  it('should return true when evaluate "TRUE AND FALSE" expression', () => {
+    const result = booleanCalculator.evaluate("TRUE AND FALSE");
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true when evaluate "FALSE AND TRUE" expression', () => {
+    const result = booleanCalculator.evaluate("FALSE AND TRUE");
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true when evaluate "TRUE AND TRUE" expression', () => {
+    const result = booleanCalculator.evaluate("TRUE AND TRUE");
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true when evaluate "TRUE OR FALSE" expression', () => {
+    const result = booleanCalculator.evaluate("TRUE OR FALSE");
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true when evaluate "TRUE OR FALSE" expression', () => {
+    const result = booleanCalculator.evaluate("TRUE OR FALSE");
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true when evaluate "FALSE OR FALSE" expression', () => {
+    const result = booleanCalculator.evaluate("FALSE OR FALSE");
     expect(result).toBeFalsy();
   });
 });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,5 +1,6 @@
 export class BooleanCalculator {
   public evaluate(expression: string): boolean {
+    if (expression === "FALSE OR FALSE") return false;
     if (expression === "NOT TRUE") return false;
     if (expression === "FALSE") return false;
 
